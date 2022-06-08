2a8741bab4165eb30a4d8014af4c15413ed791b9
gphper <570165887@qq.com>
2022-06-08 21:06:17 +0800

    fix: repair warn:The template root requires exactly one element

--- web/src/components/MainHeader.vue
+++ web/src/components/MainHeader.vue
@@ -1,12 +1,14 @@
 <template>
-    <el-row>
-        <el-col :span="4"><span class="logo">Go Redis Manager</span></el-col>
-        <el-col :span="17"></el-col>
-        <el-col :span="3"><el-button type="primary" plain @click="dialogFormVisible = true"><i class="iconfont icon-add" style="font-size:20px;">&nbsp;</i>创建Redis连接</el-button></el-col>
-    </el-row>
-    <el-dialog v-model="dialogFormVisible" title="新建Redis连接">
-        redis连接表单
-    </el-dialog>
+    <div>
+        <el-row>
+            <el-col :span="4"><span class="logo">Go Redis Manager</span></el-col>
+            <el-col :span="17"></el-col>
+            <el-col :span="3"><el-button type="primary" plain @click="dialogFormVisible = true"><i class="iconfont icon-add" style="font-size:20px;">&nbsp;</i>创建Redis连接</el-button></el-col>
+        </el-row>
+        <el-dialog v-model="dialogFormVisible" title="新建Redis连接">
+            redis连接表单
+        </el-dialog>
+    </div>
 </template>
 
 <script>
