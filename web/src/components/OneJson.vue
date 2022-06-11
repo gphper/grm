a9a596f4640ffdd48b3a6c7947366aeebf883df2
gphper <570165887@qq.com>
2022-06-11 13:54:20 +0800

    feat:add json view

--- web/src/components/OneJson.vue
+++ web/src/components/OneJson.vue
@@ -1,14 +1,36 @@
 <template>
     <div>
-        OneJson
+        <JsonViewer :value="currentData" boxed sort />
     </div>
 </template>
 
 <script>
+import {JsonViewer} from "vue3-json-viewer"
+import "vue3-json-viewer/dist/index.css";
+import { computed } from '@vue/runtime-core';
+
 export default {
     name:"OneJson",
-    setup() {
-        
+    props:{
+        data:{
+            type:String
+        }
+    },
+    components:{
+        JsonViewer
+    },
+    setup(props) {
+        // let currentData = ref(props.data)
+
+        let currentData = computed({
+            get:()=>{
+               return JSON.parse(props.data);
+            },
+        });
+
+        return {
+            currentData
+        }
     },
 }
 </script>
